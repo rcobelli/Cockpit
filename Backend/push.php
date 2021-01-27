3d4e10c06f9d9aead66d703c8f316e12f5684e75
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2021-01-27 10:43:20 -0500

    Log post to php log

--- Backend/push.php
+++ Backend/push.php
@@ -19,7 +19,7 @@ use Aws\Sns\Exception\InvalidSnsMessageException;
 $message = Message::fromRawPostData();
 $validator = new MessageValidator();
 
-error_log(print_r($_POST), 0);
+error_log("Alpha",0);
 
 // Validate the message and log errors if invalid.
 try {
@@ -27,9 +27,10 @@ try {
 } catch (InvalidSnsMessageException $e) {
    // Pretend we're not here if the message is invalid.
    http_response_code(404);
-   error_log('SNS Message Validation Error: ' . $e->getMessage());
+   error_log('SNS Message Validation Error: ' . $e->getMessage(), 0);
    die();
 }
+error_log("Bravo",0);
 
 
 // Check the type of the message and handle the subscription.
@@ -37,6 +38,12 @@ if ($message['Type'] === 'SubscriptionConfirmation') {
    // Confirm the subscription by sending a GET request to the SubscribeURL
    file_get_contents($message['SubscribeURL']);
 }
+
+error_log("Charlie",0);
+
+
+error_log($message,0);
+error_log($message['Message'],0);
 
 if ($message['Type'] === 'Notification') {
     $messageBody = $message['Message'];
