d4faec7a021aea466a96e8520a2e947cf1bc77a8
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2021-03-11 12:22:50 -0500

    Add register debug handling

--- Backend/register.php
+++ Backend/register.php
@@ -1,5 +1,13 @@
 <?php
 
+if ($_COOKIE['debug'] == 'true') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(-1);
+} else {
+    error_reporting(0);
+}
+
 $file = fopen("token.txt", "w");
 fwrite($file, $_GET['token']);
 fclose($file);
