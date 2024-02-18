7fe95787c62e534904a199b6c00d804d97204bbe
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2024-02-18 13:03:40 -0500

    Add manifest.json

--- public/index.php
+++ public/index.php
@@ -15,34 +15,42 @@ $dbHelper = new DBHelper($config);
 ?>
 
 <html>
-<style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
-body {
-    font-family: "Open Sans", sans-serif;
-}
-table td + td {
-    padding-left: 5px;
-}
-</style>
+
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+        body {
+            font-family: "Open Sans", sans-serif;
+        }
+
+        table td+td {
+            padding-left: 5px;
+        }
+    </style>
+    <link rel="manifest" href="manifest.json" />
+</head>
+
 <body>
     <h1>Cockpit</h1>
     <h2>Notifications Management</h2>
     <button id="push-subscription-button">Push notifications !</button>
     <button id="send-push-button">Send a test notification</button>
     <script type="text/javascript" src="app.js"></script>
-    <hr/>
+    <hr />
     <h2>Recent Notifications</h2>
     <table>
         <tr>
-           <th>Timestamp</th>
-           <th>Message</th>
+            <th>Timestamp</th>
+            <th>Message</th>
         </tr>
-    <?php
-    $messages = $dbHelper->getMessages();
-    foreach ($messages as $message) {
-        echo "<tr><td>" . $message['timestamp'] . "</td><td>" . $message['message'] . "</td></tr>";
-    }
-    ?>
+        <?php
+        $messages = $dbHelper->getMessages();
+        foreach ($messages as $message) {
+            echo "<tr><td>" . $message['timestamp'] . "</td><td>" . $message['message'] . "</td></tr>";
+        }
+        ?>
     </table>
 </body>
+
 </html>
